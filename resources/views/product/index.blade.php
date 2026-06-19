061c9628c84438b6b46943c0cf4c2ef03aa0593d
alidzafr <alidzafathur@gmail.com>
2026-06-19 20:00:26 +0700

    Breadcumb & Table button

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,6 +1,17 @@
 <x-layout>
     {{-- Top row title --}}
-    <div class="flex my-4 w-full justify-end">
+    <div class="flex mb-4 w-full justify-between items-center">
+        <div>
+            <div class="breadcrumbs text-sm">
+                <ul>
+                    <li><a>Home</a></li>
+                    <li>Product List</li>
+                </ul>
+            </div>
+            <div class="prose"><h1>
+                Products
+            </h1></div>
+        </div>
         <a href="{{ route('products.create') }}"
             class="btn btn-primary">
             Add new product +
@@ -16,7 +27,7 @@
                         <input type="hidden" name="category" value="{{ request('category') }}">
                     @endif
                     <div class="join">
-                        <label class="input">
+                        <label class="input rounded-tl-lg rounded-bl-lg">
                             <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                 <g stroke-linejoin="round"
                                 stroke-linecap="round" stroke-width="2.5"
@@ -27,12 +38,12 @@
                             </svg>
                             <input name="search" type="search" required placeholder="Type here" autocomplete="off"/>
                         </label>
-                        <button class="btn btn-ghost border border-gray-300 join-item">Search</button>
+                        <button class="btn btn-ghost rounded-tr-lg rounded-br-lg border border-gray-300 join-item">Search</button>
                     </div>
                 </form>
                 {{-- Category tab --}}
-                <div class="dropdown dropdown-left">
-                    <div tabindex="0" role="button" class="btn btn-ghost border border-gray-300">
+                <div class="dropdown dropdown-bottom dropdown-end">
+                    <div tabindex="0" role="button" class="btn btn-ghost rounded-lg border border-gray-300">
                         <span>Category</span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-funnel-fill" viewBox="0 0 16 16"><path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5z"/></svg>
                     </div>
@@ -79,11 +90,11 @@
                     <!-- row 1 -->
                     <tr class="border-y border-gray-200">
                         <th class="ps-4">{{ $loop->iteration }}</th>
-                        <td class="flex space-x-2">
+                        <td class="flex py-4 space-x-2 items-center">
                             <span>
                                 <img src="{{Storage::url(($product->photo))}}" alt ="album" class="w-16">
                             </span>
-                            <div class="py-4">
+                            <div>
                                 {{ $product->name }}
                             </div>
                         </td>
@@ -110,29 +121,36 @@
                         <td>
                             <div class="dropdown dropdown-bottom dropdown-end">
                                 <div tabindex="0" role="button" class="hover:cursor-pointer">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16"><path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3"/></svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16"><path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/></svg>
                                 </div>
-                                <ul tabindex="-1" class="dropdown-content menu bg-base-100 rounded-box border border-gray-200 z-1 w-50 p-3 shadow-md font-bold">
-                                    <li>
-                                        <a href="{{ route('products.edit', $product->id) }}">
+                              <div
+                                tabindex="0"
+                                class="dropdown-content card card-sm bg-base-100 z-1 w-48 shadow-md">
+                                <div class="card-body list-none">
+                                    <li class="p-2 hover:bg-gray-100">
+                                        <a href="{{ route('products.edit', $product->id) }}"
+                                            class="hover:cursor-pointer">
                                             Edit
                                         </a>
                                     </li>
-                                    <li>
-                                        <a href="{{ route('products.show', $product->id) }}">
+                                    <li class="p-2 hover:bg-gray-100">
+                                        <a href="{{ route('products.show', $product->id) }}"
+                                            class="hover:cursor-pointer">
                                             View Detail
                                         </a>
                                     </li>
-                                    <li>
+                                    <li class="p-2 hover:bg-gray-100">
                                         <form action="{{ route('products.destroy', $product->id) }}" method="POST">
                                             @method('DELETE')
                                             @csrf
-                                            <button>
+                                            <button 
+                                            class="hover:cursor-pointer">
                                                 Delete
                                             </button>
                                         </form>
                                     </li>
-                                </ul>
+                                </div>
+                              </div>
                             </div>
                         </td>
                         @endrole
