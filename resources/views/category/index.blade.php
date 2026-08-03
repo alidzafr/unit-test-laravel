bd953bc0f7c9b0c0edc5b1f8e25294dd15a4a079
alidzafr <alidzafathur@gmail.com>
2026-08-03 11:32:56 +0700

    Category Index

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -18,34 +18,49 @@
             Add New +
         </a>
     </div> --}}
-        @role('owner')
-            <div class="flex p-4 justify-end space-x-4">
-                {{-- Search Bar --}}
-                <form>
-                    <div class="join">
-                        <label class="input rounded-tl-lg rounded-bl-lg">
-                            <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <g stroke-linejoin="round"
-                                stroke-linecap="round" stroke-width="2.5"
-                                fill="none" stroke="currentColor">
-                                    <circle cx="11" cy="11" r="8"></circle>
-                                    <path d="m21 21-4.3-4.3"></path>
-                                </g>
-                            </svg>
-                            <input name="search" type="search" required placeholder="Type here" value="{{ request('search') }}" autocomplete="off"/>
-                        </label>
-                        <button class="btn btn-ghost rounded-tr-lg rounded-br-lg border border-gray-300 join-item">Search</button>
-                        @if (request()->has('search') && !is_null(request()->input('search')))
-                            <a href="{{ route('categories.index', request()->query->remove('search')) }}" 
-                                class="btn btn-soft btn-error">
-                                Clear
-                            </a>
-                        @endif
-                    </div>
-                </form>
+
+    <div class="flex mb-4 justify-between space-x-4">
+        {{-- Search Bar --}}
+        <form>
+            <div class="join">
+                <label class="input w-md rounded-l-xl">
+                    <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <g
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                        stroke-width="2.5"
+                        fill="none"
+                        stroke="currentColor">
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <path d="m21 21-4.3-4.3"></path>
+                      </g>
+                  </svg>
+                  <input name="search" type="search" required value="{{ request('search') }}" autocomplete="off"/>
+                        
+                </label>
                 
+                <button class="btn btn-soft btn-primary rounded-r-xl join-item border-2">
+                    Search
+                </button>
+
+                
+                @if (request()->has('search') && !is_null(request()->input('search')))
+                <a href="{{ route('categories.index', request()->query->remove('search')) }}" 
+                    class="ml-1 btn btn-soft btn-secondary rounded-xl border-2">
+                    Clear
+                </a>
+                @endif
+
             </div>
-        @endrole
+        </form>
+            
+            @role('owner')
+                <a href="{{ route('categories.create') }}" class="btn btn-primary rounded-xl">
+                    + Add Category
+                </a>
+            @endrole
+            
+        </div>
 
 <div class="overflow-x-auto bg-white border border-gray-200 rounded-2xl shadow">
     <table class="table">
@@ -84,10 +99,10 @@
             <td>{{ $category->created_at }}</td>
             <td>
                 <div class="flex justify-center space-x-2">
-                    <a href="{{ route('categories.show', $category->slug) }}" class="btn btn-info opacity-0 group-hover:opacity-100 transition-opacity">
+                    <a href="{{ route('categories.show', $category->slug) }}" class="btn btn-soft btn-info rounded-xl opacity-0 group-hover:opacity-100 transition-opacity">
                         View
                     </a>
-                    <a href="{{ route('categories.edit', $category->slug) }}" class="btn btn-warning opacity-0 group-hover:opacity-100 transition-opacity">
+                    <a href="{{ route('categories.edit', $category->slug) }}" class="btn btn-soft btn-warning rounded-xl opacity-0 group-hover:opacity-100 transition-opacity">
                         Edit
                     </a>
                 </div>
