e9c0da8780c0048c87c9279e5680c5850781639f
alidzafr <alidzafathur@gmail.com>
2026-08-08 05:55:34 +0700

    customers controller

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -2,17 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Customers;
+use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 
 class CustomersController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $customers = Customer::filter(request(['search']))
+            ->latest()
+            ->paginate(10)
+            ->withQueryString();
+
+        return view('customer.index', compact('customers'));
     }
 
     /**
@@ -20,7 +28,7 @@ class CustomersController extends Controller
      */
     public function create()
     {
-        //
+        return view('customer.create');
     }
 
     /**
@@ -28,38 +36,101 @@ class CustomersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|max:50|string',
+            'phone' => 'required|max:10000000000|numeric',
+            'email' => 'required|max:50|string|unique:customer,email',
+            'address' => 'required|max:50|string'
+        ], [
+            'emaiil.unique' => 'Email sudah digunakan'
+        ]);
+
+        try {
+            $validated['slug'] = Str::slug($request->name);
+            
+            $newCustomer = Customer::create($validated);
+            DB::commit();
+
+            return redirect()->route('customer.index')
+                ->with('success', 'Customer created successfully');
+        } catch (\Exception $e) {
+            DB::rollback();
+
+            $error = ValidationException::withMessages([
+                'system_error' => ['System error!' . $e->getMessage()],
+            ]);
+            throw $error;
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Customers $customers)
+    public function show(Customer $customer)
     {
-        //
+        return view('customer.show', compact('customer'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Customers $customers)
+    public function edit(Customer $customer)
     {
-        //
+        return view('customer.edit', compact('customer'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Customers $customers)
+    public function update(Request $request, Customer $customer)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|max:50|string',
+            'phone' => 'required|max:10000000000|numeric',
+            'address' => 'required|max:50|string'
+        ]);
+
+        DB::beginTransaction()
+
+        try {
+            // name must unique or same as previous
+            if ($request['email'] == $customer->email) {
+                $request->validate(['email' => 'required|max:50|string']);
+                $validated['email'] =  $request['email'];
+            } else {
+                $request->validate(
+                    ['email' => 'required|max:50|string|unique:customers,email'],
+                    ['email.unique' => 'Nama Kategori sudah digunakan.']
+                );
+                $validated['email'] = $request['email'];
+            }
+
+            // slug
+            $validated['slug'] = Str::slug($request->name);
+            $customer->update($validated);
+
+            DB::commit();
+
+            return redirect()->route('customer.show', $customer->slug)
+                ->with('success', 'Customer updated successfully');
+        } catch (\Exception $e) {
+            DB::rollback();
+
+            $error = ValidationException::withMessages([
+                'system_error' => ['System error!' . $e->getMessage()],
+            ]);
+            throw $error;
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Customers $customers)
+    public function destroy(Customer $customer)
     {
-        //
+        Customer::where('customer', $customer)->delete();
+
+        return redirect()->route('customer.index')
+            ->with('success', 'customer deleted successfully');
     }
 }
---
+++ app/Models/Customer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    protected $fillable = [
+        'name',
+        'phone',
+        'email',
+        'address'
+    ];
+
+    public function filter(Builder $query, array $filters)
+    {
+        $query->when(
+            $filters['search'] ?? false,
+            fn($query, $search) =>
+            $query->where('name', 'like', '%' . $search . '%')
+        );
+    }
+}
