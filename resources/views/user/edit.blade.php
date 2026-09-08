73b7b4f9ecde8db0206c0e91380143ff3445ee0b
alidza <alidzafathur@gmail.com>
2026-09-08 17:41:46 +0700

    setting menu

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -7,112 +7,101 @@
 
         <div class="grid gap-6 lg:grid-cols-[1.3fr_0.7fr]">
             {{-- Card --}}
+            <div class="flex items-center gap-3 border-b border-gray-100 pb-4">
+                <div class="flex-1">
+                    <h3 class="text-lg font-semibold text-gray-900">Informasi Pengguna</h3>
+                    <p class="text-sm text-gray-500">Ubah nama, email dan password pengguna.</p>
+                </div>
+                <label for="my_modal_6" class="btn btn-soft btn-error rounded-xl">Hapus pengguna</label>
+            </div>
+            
+            @if ($errors->any())
+            <div role="alert" class="mb-2 alert alert-error">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                </svg>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+            
             <form action="{{ route('users.update', $user) }}" method="post"
                 class="rounded-2xl border border-gray-200 bg-white p-6 shadow-sm">
                 @method("PUT")
                 @csrf
-                <div class="flex items-center gap-3 border-b border-gray-100 pb-4">
-                    <div class="flex-1">
-                        <h3 class="text-lg font-semibold text-gray-900">Informasi Pengguna</h3>
-                        <p class="text-sm text-gray-500">Ubah nama, email dan password pengguna.</p>
+                
+                <fieldset class="fieldset">
+                    <legend class="fieldset-legend">Nama Pengguna</legend>
+                    <input 
+                        name="name" type="text" value="{{ $user->name }}"
+                        class="input rounded-xl w-full @error('name') outline-2 outline-red-500 @enderror" 
+                        placeholder="Type here"
+                    />
+                    @error('name')
+                        <div class="label text-sm text-red-600">{{ $message }}</div>
+                    @enderror
+                </fieldset>
+                
+                {{-- Email --}}
+                <fieldset class="fieldset">
+                    <legend class="fieldset-legend">Email</legend>
+                    <input 
+                        name="email" type="email" value="{{ $user->email }}"
+                        class="input rounded-xl w-full @error('email') outline-2 outline-red-500 @enderror" 
+                        placeholder="Type here"
+                    />
+                    @error('email')
+                        <div class="label text-sm text-red-600">{{ $message }}</div>
+                    @enderror
+                </fieldset>
+
+                {{-- Role --}}
+                <fieldset class="fieldset">
+                    <legend class="fieldset-legend">Roles</legend>
+                    <div class="flex items-center gap-4">
+                        <select name="role" class="select w-full rounded-xl @error('role') outline-2 outline-red-500 @enderror">
+                            <option selected>{{ $user->getRoleNames()->first() }}</option>
+                            @foreach ($roles as $role)
+                                @if ($role->name == $user->getRoleNames()->first())
+                                    @continue {{-- Skips the rest of the loop for this specific item --}}
+                                @endif
+                                <option value="{{ $role->name }}">{{ $role->name }}</option>
+                            @endforeach
+                        </select>
                     </div>
-                    <label for="my_modal_6" class="btn btn-soft btn-error rounded-xl">Hapus pengguna</label>
-                </div>
+                    @error('role')
+                        <div class="label text-sm text-red-600">{{ $message }}</div>
+                    @enderror
+                </fieldset>
 
-                @if ($errors->any())
-                <div role="alert" class="mb-2 alert alert-error">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-                    </svg>
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-                @endif
-
-                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="col-span-full">
-                        <label for="name" class="block text-sm font-medium text-gray-900">
-                            Username
-                        </label>
-                        <div class="mt-2">
-                            <input id="name" type="text" 
-                            name="name" autocomplete="name" 
-                            class="@error('name') outline-red-500 @else outline-gray-300 @enderror block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" 
-                            value="{{ $user->name }}"/>
-
-                            @error('name')
-                            <p class="mt-1 text-error">
-                                {{ $message }}
-                            </p>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="col-span-full">
-                        <label for="email" class="block text-sm font-medium text-gray-900">
-                            Email
-                        </label>
-                        <div class="mt-2">
-                            <input id="email" type="email" 
-                            name="email" autocomplete="email" 
-                            class="@error('email') outline-red-500 @else outline-gray-300 @enderror block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" 
-                            value="{{ $user->email }}"/>
-
-                            @error('email')
-                            <p class="mt-1 text-error">
-                                {{ $message }}
-                            </p>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <fieldset class="fieldset">
-                        <legend class="fieldset-legend">Roles</legend>
-                        <div class="flex items-center gap-4">
-                            <select name="role" class="select w-full rounded-xl @error('role') outline-2 outline-red-500 @enderror">
-                                <option selected>{{ $user->getRoleNames()->first() }}</option>
-                                @foreach ($roles as $role)
-                                    @if ($role->name == $user->getRoleNames()->first())
-                                        @continue {{-- Skips the rest of the loop for this specific item --}}
-                                    @endif
-                                    <option value="{{ $role->name }}">{{ $role->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        @error('role')
-                            <div class="label text-sm text-red-600">{{ $message }}</div>
-                        @enderror
-                    </fieldset>
-
-                    {{-- Password --}}
-                    <fieldset class="fieldset">
-                        <legend class="fieldset-legend">Password</legend>
-                        <input 
-                            name="password" type="password" value="{{ old('password') }}"
-                            class="input rounded-xl w-full @error('password') outline-2 outline-red-500 @enderror" 
-                            placeholder="Type here"
-                        />
-                        @error('password')
-                            <div class="label text-sm text-red-600">{{ $message }}</div>
-                        @enderror
-                    </fieldset>
-                    
-                    {{-- Password Confirmation--}}
-                    <fieldset class="fieldset">
-                        <legend class="fieldset-legend">Password Confirmation</legend>
-                        <input 
-                            name="password_confirmation" type="password" value="{{ old('password_confirmation') }}"
-                            class="input rounded-xl w-full @error('password') outline-2 outline-red-500 @enderror" 
-                            placeholder="Type here"
-                        />
-                        @error('password')
-                            <div class="label text-sm text-red-600">{{ $message }}</div>
-                        @enderror
-                    </fieldset>
-                </div>
+                {{-- Password --}}
+                <fieldset class="fieldset">
+                    <legend class="fieldset-legend">Password</legend>
+                    <input 
+                        name="password" type="password" value="{{ old('password') }}"
+                        class="input rounded-xl w-full @error('password') outline-2 outline-red-500 @enderror" 
+                        placeholder="Type here"
+                    />
+                    @error('password')
+                        <div class="label text-sm text-red-600">{{ $message }}</div>
+                    @enderror
+                </fieldset>
+                
+                {{-- Password Confirmation--}}
+                <fieldset class="fieldset">
+                    <legend class="fieldset-legend">Password Confirmation</legend>
+                    <input 
+                        name="password_confirmation" type="password" value="{{ old('password_confirmation') }}"
+                        class="input rounded-xl w-full @error('password') outline-2 outline-red-500 @enderror" 
+                        placeholder="Type here"
+                    />
+                    @error('password')
+                        <div class="label text-sm text-red-600">{{ $message }}</div>
+                    @enderror
+                </fieldset>
 
                 <div class="flex flex-wrap mt-8 gap-3">
                     <button type="submit" class="btn btn-primary rounded-xl">
--- resources/views/user/setting.blade.php
+++ resources/views/user/setting.blade.php
@@ -3,83 +3,151 @@
         <div class="overflow-x-auto bg-white border border-gray-200 rounded-2xl shadow">
             <!-- name of each tab group should be unique -->
             <div class="tabs tabs-lift">
-                <input type="radio" name="my_tabs_3" class="tab" aria-label="Profil Pengguna" checked="checked" />
+                <input type="radio" id="tab1" name="my_tabs_3" class="tab" aria-label="Profil Pengguna" @if(! $errors->has('password')) checked="checked" @endif />
+                
+                {{-- Content 1 --}}
                 <div class="tab-content bg-base-100 border-base-300 p-6">
-                    {{-- Content 1 --}}
                     <div class="mb-6">
                         <h3 class="text-lg font-semibold text-gray-900">Profile</h3>
                         <p class="text-sm text-gray-500">Ubah nama, email dan Role Akses pengguna.</p>
                     </div>
 
-                    {{-- Grid --}}
-                    <div class="w-3xl grid gap-6 lg:grid-cols-1">
-                        <fieldset class="fieldset">
-                            <legend class="fieldset-legend">Nama</legend>
-                            <input 
-                                name="name" type="text" value="{{ $user->name }}"
-                                class="input rounded-xl w-full @error('name') outline-2 outline-red-500 @enderror" 
-                                placeholder="Type here"
-                            />
-                            @error('name')
-                                <div class="label text-sm text-red-600">{{ $message }}</div>
-                            @enderror
-                        </fieldset>
+                    <form action="{{ route('users.update', $user) }}" method="post">
+                        @method("PUT")
+                        @csrf
+                        {{-- Grid --}}
+                        <div class="w-3xl grid gap-6 lg:grid-cols-1">
+                            <fieldset class="fieldset">
+                                <legend class="fieldset-legend">Nama</legend>
+                                <input 
+                                    name="name" type="text" value="{{ $user->name }}"
+                                    class="input rounded-xl w-full @error('name') outline-2 outline-red-500 @enderror" 
+                                    placeholder="Type here"
+                                />
+                                @error('name')
+                                    <div class="label text-sm text-red-600">{{ $message }}</div>
+                                @enderror
+                            </fieldset>
 
-                        <fieldset class="fieldset">
-                            <legend class="fieldset-legend">Email</legend>
-                            <input 
-                                name="email" type="email" value="{{ $user->email }}"
-                                class="input rounded-xl w-full @error('email') outline-2 outline-red-500 @enderror" 
-                                placeholder="Type here"
-                            />
-                            @error('email')
+                            <fieldset class="fieldset">
+                                <legend class="fieldset-legend">Email</legend>
+                                <input 
+                                    name="email" type="email" value="{{ $user->email }}"
+                                    class="input rounded-xl w-full @error('email') outline-2 outline-red-500 @enderror" 
+                                    placeholder="Type here"
+                                />
+                                @error('email')
+                                    <div class="label text-sm text-red-600">{{ $message }}</div>
+                                @enderror
+                            </fieldset>
+                            
+                            <fieldset class="fieldset">
+                                <legend class="fieldset-legend">Roles</legend>
+                                <div class="flex items-center gap-4">
+                                    <select name="role" class="select w-full rounded-xl @error('role') outline-2 outline-red-500 @enderror">
+                                        <option selected>{{ $user->getRoleNames()->first() }}</option>
+                                        @foreach ($roles as $role)
+                                            @if ($role->name == $user->getRoleNames()->first())
+                                                @continue {{-- Skips the rest of the loop for this specific item --}}
+                                            @endif
+                                            <option value="{{ $role->name }}">{{ $role->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                @error('role')
                                 <div class="label text-sm text-red-600">{{ $message }}</div>
-                            @enderror
-                        </fieldset>
-                        
-                        <fieldset class="fieldset">
-                            <legend class="fieldset-legend">Roles</legend>
-                            <div class="flex items-center gap-4">
-                                <select name="role" class="select w-full rounded-xl @error('role') outline-2 outline-red-500 @enderror">
-                                    <option selected>{{ $user->getRoleNames()->first() }}</option>
-                                    @foreach ($roles as $role)
-                                        @if ($role->name == $user->getRoleNames()->first())
-                                            @continue {{-- Skips the rest of the loop for this specific item --}}
-                                        @endif
-                                        <option value="{{ $role->name }}">{{ $role->name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            @error('role')
-                            <div class="label text-sm text-red-600">{{ $message }}</div>
-                            @enderror
-                        </fieldset>
-                        
-                    </div>
+                                @enderror
+                            </fieldset>
+                            
+                        </div>
 
-                    {{-- Save row --}}
-                    <div class="my-6">
-                        <button type="submit" class="btn btn-primary mt-2">Save</button>
-                    </div>
+                        {{-- Save row --}}
+                        <div class="my-6">
+                            <button type="submit" class="btn btn-primary mt-2">Save</button>
+                        </div>
+                    </form>
 
                     {{-- Delete row --}}
                     <div>
                         <h3 class="text-lg font-semibold text-gray-900">Hapus Akun</h3>
                         <p class="text-sm text-gray-500">Hapus akun anda beserta seluruh datanya.</p>
-                        <button type="submit" class="btn btn-error mt-2">Hapus Akun</button>
+                        <label for="my_modal_6" class="btn btn-soft btn-error rounded-xl">Hapus Akun</label>
                     </div>
 
                 </div>
 
-                <input type="radio" name="my_tabs_3" class="tab" aria-label="Password" />
+                <input type="radio" id="tab2" name="my_tabs_3" class="tab" aria-label="Password" @if($errors->has('password')) checked="checked" @endif />
+
+                {{-- Content 2 --}}
                 <div class="tab-content bg-base-100 border-base-300 p-6">
-                    Content 2
+                    <div class="mb-6">
+                        <h3 class="text-lg font-semibold text-gray-900">Password</h3>
+                        <p class="text-sm text-gray-500">Ubah password pengguna.</p>
+                    </div>
+
+
+                    <form action="{{ route('users.update', $user) }}" method="post">
+                        @method("PUT")
+                        @csrf
+                        <input type="hidden" name="name" value="{{ $user->name }}">
+                        <input type="hidden" name="role" value="{{ $user->getRoleNames()->first() }}">
+
+                        {{-- Grid --}}
+                        <div class="w-3xl grid gap-6 lg:grid-cols-1">
+                            <fieldset class="fieldset">
+                                <legend class="fieldset-legend">Password</legend>
+                                <input 
+                                    name="password" type="password" value="{{ old('password') }}"
+                                    class="input rounded-xl w-full @error('password') outline-2 outline-red-500 @enderror" 
+                                    placeholder="Type here"
+                                />
+                                @error('password')
+                                    <div class="label text-sm text-red-600">{{ $message }}</div>
+                                @enderror
+                            </fieldset>
+                            
+                            <fieldset class="fieldset">
+                                <legend class="fieldset-legend">Password Confirmation</legend>
+                                <input 
+                                    name="password_confirmation" type="password" value="{{ old('password_confirmation') }}"
+                                    class="input rounded-xl w-full @error('password') outline-2 outline-red-500 @enderror" 
+                                    placeholder="Type here"
+                                />
+                                @error('password')
+                                    <div class="label text-sm text-red-600">{{ $message }}</div>
+                                @enderror
+                            </fieldset>
+                        </div>
+
+                        {{-- Save row --}}
+                        <div class="my-6">
+                            <button type="submit" class="btn btn-primary mt-2">Save</button>
+                        </div>
+                    </form>
                 </div>
 
-                <input type="radio" name="my_tabs_3" class="tab" aria-label="Tema Tampilan" />
+                {{-- <input type="radio" name="my_tabs_3" class="tab" aria-label="Tema Tampilan" />
                 <div class="tab-content bg-base-100 border-base-300 p-6">
                     Content 3
-                </div>
+                </div> --}}
+            </div>
+        </div>
+    </div>
+
+    <!-- Put this part before </body> tag -->
+    <input type="checkbox" id="my_modal_6" class="modal-toggle" />
+    <div class="modal" role="dialog">
+        <div class="modal-box">
+            <h3 class="text-lg font-bold">Perhatian!</h3>
+            <p class="py-4">Apakah anda yakin untuk menghapus pengguna ?</p>
+            
+            <div class="modal-action">
+                <form action="{{ route('users.destroy', $user ) }}" method="POST">
+                    @method('DELETE')
+                    @csrf
+                    <button class="btn btn-error">Hapus</button>
+                </form>
+                <label for="my_modal_6" class="btn">Batal</label>
             </div>
         </div>
     </div>
